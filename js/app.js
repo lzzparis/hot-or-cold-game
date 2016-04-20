547236a37fb40b06a643989f4bd64e2fa940ea16
Lizzie Paris <lzz.paris@gmail.com>
2016-04-19 22:04:53 -0700

    add feedback, update newGame for clearing DOM updates

--- js/app.js
+++ js/app.js
@@ -24,6 +24,11 @@ function newGame (){
 	goldenNum = Math.floor((Math.random()*100)+1);
 	console.log(goldenNum);
 	counter = 0;
+	$("#count").text(counter);
+	$("#userGuess").val("");
+	$("#guessList").text("");
+	$("#feedback").text("Make your Guess!");
+	
 }
 function registerGuess(event){
 	event.preventDefault();
@@ -33,8 +38,28 @@ function registerGuess(event){
 	counter++;
 	$("#count").text(counter);
 	$("#guessList").append("<li>"+currentGuess+"</li>")
-
+	giveFeedback(currentGuess);
 }
 
-
+function giveFeedback(guessNum){
+	var feedbackText = "freezing cold";
+	var delta = Math.abs(guessNum - goldenNum);
+	if (delta <= 10){
+		feedbackText="boiling hot!";
+	}
+	else if (delta <= 20) {
+		feedbackText="hot";
+	}
+	else if (delta <= 30) {
+		feedbackText="warm";
+	}
+	else if(delta <= 50){
+		feedbackText="cold";
+	}
+	else {
+		feedbackText="freezing cold";
+	}
+	console.log(feedbackText);
+	$("#feedback").text(feedbackText);
+}
 
